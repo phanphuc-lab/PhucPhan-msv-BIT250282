f7f8ef4ba72cc31b9f51375b0478ab4cdf368791
phanphuc-lab <phanphucxo25@gmail.com>
2026-03-03 23:35:32 +0700

    Create bai15.py

---
+++ bai15.py
@@ -0,0 +1,12 @@
+cah 1
+s = input("Nhập chuỗi: ")
+dao = ""
+for ch in s:
+    dao = ch + dao
+print("Chuỗi đảo ngược:", dao)
+
+
+
+cah 2
+s = input("Nhập chuỗi: ")
+print("Chuỗi đảo ngược:", s[::-1])
