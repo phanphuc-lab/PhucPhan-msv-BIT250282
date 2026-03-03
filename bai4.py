38313186d263ebc1093e599920a46df114a03820
phanphuc-lab <phanphucxo25@gmail.com>
2026-03-03 23:31:38 +0700

    Create bai4.py

---
+++ bai4.py
@@ -0,0 +1,7 @@
+numbers = [12, 5, 8, 5, 20, 1]
+print("Danh sách ban đầu:", numbers)
+numbers.sort()
+print("Sắp xếp tăng dần:", numbers)
+numbers.reverse()
+print("Đảo ngược danh sách:", numbers)
+print("Số lần số 5 xuất hiện:", numbers.count(5))
